c40b841d3619fbd8fb9845d5c12fa1b5fad7aa0d
smcurry83 <125812515+smcurry83@users.noreply.github.com>
2023-03-24 19:48:29 -0400

    Remove unused code and clean up score dialogue

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let playerScore = 0;
 
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 100) +1;
-  //console.log(random);
   if (random <= 33) {
     computerChoice = "Rock";
   } else if (random >= 66) {
@@ -49,28 +48,14 @@ function playRound (computerChoice, playerChoice) {
   return computerScore, playerScore;
 }
 
-// function compareScore(computerScore, playerScore) {
-//   let topScore = "";
-//   if (computerScore > playerScore) {
-//     topScore = computerScore;
-//   } else if (computerScore < playerScore) {
-//     topScore = playerScore;
-//   } else {
-//     topScore = computerScore;
-//   }
-//   return topScore;
-// }
-
 function game () {
-  //let score = compareScore(computerScore, playerScore);
-
   while ((computerScore < 5) && (playerScore < 5)) {
     getComputerChoice();
     getPlayerChoice();
     console.log("Computer chose: " + computerChoice);
     console.log("You chose: " + playerChoice);
     playRound(computerChoice, playerChoice);
-    console.log("The score is: Computer: " + computerScore + ". Your score: " + playerScore);
+    console.log("The score is: Computer: " + computerScore + " - You: " + playerScore);
     if (computerScore === 5) {
       console.log("COMPUTER WINS!!!");
     } else if (playerScore === 5) {
